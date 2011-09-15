71f02f64347b5985c88b3002b18aea15be0771ca
ricmercado <slipstream_ric08@yahoo.com>
2011-09-15 17:09:14 +0800

    new app

---
+++ poc/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Poc::Application.config.secret_token = 'c8958b9a45928399c905c33d411ffeef9bd81821e39fbeaf1be0c54ef210dcb0461cda6ae8c1121dce692efece8a85a9f93b1d2316b72786b24da8aae54ce26c'
